b67fe83735da16e17003dcaefee02c536aff6866
Juel07 <juel.braganza97@gmail.com>
2021-07-06 23:02:49 +0100

    complete Business Logic #6-#9 & Interface

--- Thermostat/spec/thermostatSpec.js
+++ Thermostat/spec/thermostatSpec.js
@@ -1,33 +1,87 @@
-describe('Thermostat', function() {
+describe('Thermostat', () => {
     let thermostat;
 
-    beforeEach( function(){
+    beforeEach(() => {
         thermostat = new Thermostat();
-    })
-
-    it("starts at 20 deg", function(){
-        expect(thermostat.temperature).toEqual(20)
-    })
-    it("up() increases temp by 1 deg", function(){
-        thermostat.up()
-        expect(thermostat.temperature).toEqual(21)
-    })
-    it("down() decreases temp by 1 deg", function(){
-        thermostat.down()
-        expect(thermostat.temperature).toEqual(19)
-    })
-    it("does not go below 10 deg", function(){
-        for (let i = 0; i < 15; i +=1) {
-            thermostat.down()
-        }
-        expect(thermostat.temperature).toEqual(10)
-    })
-
-    it("maximum temperature is 25 deg if power saving is on", function() {
-      thermostat.turnPowerSavingOn();
-      for (let i = 0; i < 10; i +=1) {
-        thermostat.up()
-      }
-      expect(thermostat.temperature).toEqual(25)
     });
-} )
+
+    it("starts at 20 deg", () => {
+        expect(thermostat.getCurrentTemperature()).toEqual(20);
+    });
+
+    describe("Temperature Controls", () => {
+        it("up() increases temp by 1 deg", () => {
+            thermostat.up();
+            expect(thermostat.getCurrentTemperature()).toEqual(21);
+        });
+
+        it("down() decreases temp by 1 deg", () => {
+            thermostat.down();
+            expect(thermostat.getCurrentTemperature()).toEqual(19);
+        });
+
+        it("temp does not go below 10 deg", () => {
+            for (let i = 0; i < 15; i += 1) {
+                thermostat.down();
+            }
+            expect(thermostat.getCurrentTemperature()).toEqual(10);
+        });
+
+        it("reset() changes the temp back to default, 20 deg", () => {
+            for (let i = 0; i < 5; i += 1) {
+                thermostat.up();
+            }
+            thermostat.resetTemperature()
+            expect(thermostat.getCurrentTemperature()).toEqual(20);
+        })
+    });
+
+    describe("Power Saving Mode", () => {
+        it("power saving mode is on by default", () => {
+            expect(thermostat.getPowerSavingMode()).toEqual(true);
+        });
+
+        it("max temp can be 25 deg when PSM is switched on", () => {
+            for (let i = 0; i < 10; i += 1) {
+                thermostat.up();
+            }
+            expect(thermostat.getCurrentTemperature()).toEqual(25)
+        });
+
+        it('power saving mode can be switched off', () => {
+            thermostat.switchOffPowerSavingMode()
+            expect(thermostat.getPowerSavingMode()).toEqual(false)
+        });
+
+        it('max temp can be 32 deg when PSM is switched off', () => {
+            thermostat.switchOffPowerSavingMode()
+            for (let i = 0; i < 14; i += 1) {
+                thermostat.up()
+            }
+            expect(thermostat.getCurrentTemperature()).toEqual(32)
+
+        });
+    });
+
+    describe("Display usage levels", () => {
+        it("temp less than 18 deg is low usage", () => {
+            for (let i = 0; i < 5; i += 1) {
+                thermostat.down();
+            }
+            expect(thermostat.currentEnergyUsage()).toEqual('low usage');
+        });
+
+        it("temp less than 18 & less than 25 deg is medium usage", () => {
+            expect(thermostat.currentEnergyUsage()).toEqual('medium usage');
+        });
+
+        it("temp greater 25 deg is high usage", () => {
+            thermostat.powerSavingModeOn = false;
+            for (let i = 0; i < 10; i += 1) {
+                thermostat.up();
+            }
+            expect(thermostat.currentEnergyUsage()).toEqual('high usage');
+        });
+    });
+
+})
---
+++ Thermostat/src/js/thermostat.js
@@ -0,0 +1,68 @@
+'use strict';
+
+class Thermostat {
+  constructor() {
+    this.MIN_TEMP = 10;
+    this.DEFAULT_TEMP = 20;
+    this.temperature = this.DEFAULT_TEMP;
+    this.MAX_TEMP_PSM_ON = 25;
+    this.MAX_TEMP_PSM_OFF = 32;
+    this.powerSavingModeOn = true;
+    this.MEDIUM_USAGE_TEMP_LIMIT = 18;
+    this.HIGH_USAGE_TEMP_LIMIT = 25;
+  }
+
+  up() {
+    if (!this.isMaximumTemperature()) {
+      this.temperature += 1;
+    }
+    return;
+  }
+
+  down() {
+    if (!this.isMinimumTemperature()) {
+      this.temperature -= 1;
+    }
+    return;
+  }
+
+  switchOffPowerSavingMode() {
+    return this.powerSavingModeOn = false;
+  }
+
+  resetTemperature() {
+    return this.temperature = this.DEFAULT_TEMP;
+  }
+
+  currentEnergyUsage() {
+    if (this.temperature < this.MEDIUM_USAGE_TEMP_LIMIT) {
+      return 'low usage';
+    };
+    if (this.temperature <= this.HIGH_USAGE_TEMP_LIMIT) {
+      return 'medium usage';
+    };
+    if (this.temperature > this.HIGH_USAGE_TEMP_LIMIT) {
+      return 'high usage';
+    };
+  }
+
+  isMaximumTemperature() {
+    if (!this.getPowerSavingMode()) {
+      return this.temperature === this.MAX_TEMP_PSM_OFF;
+    }
+    return this.temperature === this.MAX_TEMP_PSM_ON;
+  }
+
+  isMinimumTemperature() {
+    return this.temperature === this.MIN_TEMP;
+  }
+
+  getCurrentTemperature() {
+    return this.temperature;
+  }
+
+  getPowerSavingMode() {
+    return this.powerSavingModeOn;
+  }
+
+}
